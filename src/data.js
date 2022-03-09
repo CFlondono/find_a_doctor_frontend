6b16d7e29ae7ccd22e04316634bc237ae7738217
Claudia Londono <claudia@londonos.com>
2022-03-09 16:56:51 -0600

    Static data and initial UI

---
+++ src/data.js
@@ -0,0 +1,78 @@
+export default[
+    {
+        "_id": "6226ed2634df2c39490d973c",
+        "name": "Juan",
+        "lastname": "Fernandez",
+        "specialities": ["cardiologiys"],
+        "languages": ["Spanish", "French"],
+        "education": ["UT", "OU"],
+        "reviews": [],
+        "__v": 0
+    },
+    {
+        "_id": "6227c0150e327b73d8aa60c4",
+        "name": "Juan",
+        "lastname": "Londono",
+        "specialities": [
+            "neurologist",
+            "family"
+        ],
+        "languages": [
+            "Spanish",
+            "Portuguese"
+        ],
+        "education": [],
+        "reviews": [],
+        "__v": 0
+    },
+    {
+        "_id": "6227d2d67fa208898d72e36f",
+        "name": "Carlos",
+        "lastname": "De Los Reyes",
+        "specialities": [
+            "physiatrist"
+        ],
+        "languages": [
+            "Spanish", "German"
+        ],
+        "education": [
+            "U de C"
+        ],
+        "reviews": [],
+        "__v": 0
+    },
+    {
+        "_id": "6228d04f80e8520c5ef8df75",
+        "name": "Alejandro",
+        "lastname": "Lopez",
+        "specialities": [
+            "Ortopedist",
+            "gediatric"
+        ],
+        "languages": [
+            "Spanish","English"
+        ],
+        "education": ["UT",
+            "U de C"
+        ],
+        "reviews": [],
+        "__v": 0
+    },
+    {
+        "_id": "6228d14880e8520c5ef8df77",
+        "name": "Alejandro",
+        "lastname": "Londono",
+        "specialities": [
+            "Ortopedist",
+            "gediatric"
+        ],
+        "languages": [
+            "Spanish"
+        ],
+        "education": [
+            "U de C","UH"
+        ],
+        "reviews": [],
+        "__v": 0
+    }
+]
